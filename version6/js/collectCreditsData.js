e4938c30ee1ba81791893ee5004d2aeb4a3f1e9d
damienPilat <dspilat@hotmail.com>
2021-02-03 13:47:45 +0000

    Video Popup: Get Video Key

--- version6/js/collectCreditsData.js
+++ version6/js/collectCreditsData.js
@@ -9,14 +9,18 @@ async function creditsData(movieData) {
     let movie_id = Object.keys(movieData);
 
     for (let i=0; i<movie_id.length; i++) {
-        let moive_credits_url = "https://api.themoviedb.org/3/movie/" + movie_id[i] + "/credits?api_key=54f244c3bc41ade17bb0dcfd25aab606&language=en-US";
+
+        // Get Youtube Video Key
+        let movie_video_url = "https://api.themoviedb.org/3/movie/" + movie_id[i] + "/videos?api_key=54f244c3bc41ade17bb0dcfd25aab606&language=en-US";
+        await apiCall(movie_video_url)
+            .then(result => { movieData[movie_id[i]]['video_id'] = result['results'][0]['key']; });
 
         // Make TMDb API call, then store general cast/crew details, then store cast/crew statistics
+        let moive_credits_url = "https://api.themoviedb.org/3/movie/" + movie_id[i] + "/credits?api_key=54f244c3bc41ade17bb0dcfd25aab606&language=en-US";
         await apiCall(moive_credits_url)
             .then(value => creditsDetails(value, movieData, movie_id[i]))
             .then(value => castStats(value, movie_id[i]));
     }
-
     return movieData
 } // END: creditsData
 
