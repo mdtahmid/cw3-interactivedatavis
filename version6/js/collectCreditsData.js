4ac1a2ed8cdeda0dcd6c5a54eb6579b27606fa90
damienPilat <dspilat@hotmail.com>
2021-02-05 14:11:34 +0000

    Collect and Populate Watch Providers Data

--- version6/js/collectCreditsData.js
+++ version6/js/collectCreditsData.js
@@ -10,6 +10,11 @@ async function creditsData(movieData) {
 
     for (let i=0; i<movie_id.length; i++) {
 
+        // Get Watch Providers
+        let movie_watch_providers = "https://api.themoviedb.org/3/movie/" + movie_id[i] + "/watch/providers?api_key=54f244c3bc41ade17bb0dcfd25aab606&language=en-US";
+        await apiCall(movie_watch_providers)
+                .then(resultData => getWatchProviders(resultData, movieData, movie_id[i]));
+
         // Get Youtube Video Key
         let movie_video_url = "https://api.themoviedb.org/3/movie/" + movie_id[i] + "/videos?api_key=54f244c3bc41ade17bb0dcfd25aab606&language=en-US";
         await apiCall(movie_video_url)
@@ -24,6 +29,37 @@ async function creditsData(movieData) {
     return movieData
 } // END: creditsData
 
+function getWatchProviders(resultData, movieData, movieId) {
+    // MUST attribute the source of the data as JustWatch
+    let poster_link = "https://www.themoviedb.org/t/p/original/";
+
+    let buy_providers = resultData.results.GB.buy;
+    let flatrate_providers = resultData.results.GB.flatrate;
+    let rent_providers = resultData.results.GB.rent;
+
+    let buy_details = [];
+    if (buy_providers) {
+        buy_providers.forEach(provider => {
+            buy_details.push({ name: provider.provider_name, logo_path: poster_link + provider.logo_path });
+        });
+    }
+
+    let flatrate_details = [];
+    if (flatrate_providers) {
+        flatrate_providers.forEach(provider => {
+            flatrate_details.push({ name: provider.provider_name, logo_path: poster_link + provider.logo_path });
+        });
+    }
+
+    let rent_details = [];
+    if (rent_providers) {
+        rent_providers.forEach(provider => {
+            rent_details.push({ name: provider.provider_name, logo_path: poster_link + provider.logo_path });
+        });
+    }
+
+    movieData[movieId]['watch_providers'] = { buy_providers: buy_details, flatrate_providers: flatrate_details, rent_providers: rent_details};
+}
 
 // Store cast & crew details
 function creditsDetails(data, movieData, item) {
